9a2128ce1b3ef9dab8e7f4b22d414a86426fa1e8
Lura Skye <l@veriny.tf>
2022-11-26 07:33:10 +0000

    refactor + ep 001

--- makeup.py
+++ makeup.py
@@ -1,6 +1,7 @@
 """
 Makes up the static website.
 """
+import os
 import shutil
 import time
 from pathlib import Path
@@ -21,10 +22,22 @@ def raise_helper(msg):
 before = time.monotonic()
 env = Environment(loader=FileSystemLoader(str(our_path / "templates")))
 env.globals["raise"] = raise_helper
-template = env.get_template("root.html")
+template = env.get_template("content.html")
 data = template.render()
 
 (out_dir / "index.html").write_text(data)
+
+reviews_path = our_path / "templates" / "reviews"
+reviews_out = out_dir / "reviews"
+reviews_out.mkdir()
+
+for f in reviews_path.iterdir():
+    if not f.name.startswith("_") and f.name.endswith(".html"):
+        template = env.get_template(f"reviews/{f.name}")
+        data = template.render()
+        (reviews_out / f.name).write_text(data)
+
+
 shutil.copytree(our_path / "static", out_dir / "static")
 
 after = time.monotonic()
