a41bb4703843798022b5a4180a485d8d50a45a00
Lura Skye <l@veriny.tf>
2022-12-10 01:11:38 +0000

    episode 41 review + update next ep script

--- next-ep.py
+++ next-ep.py
@@ -5,11 +5,15 @@ import os
 import sys
 from pathlib import Path
 
-REVIEW_TEMPLATE = """{% from "reviews/_macros.html" import review_header, review_footer %}
+REVIEW_TEMPLATE = """{% extends "_helpers/review.html" %}
+{% from "_helpers/images.jinja2" import iimg %}
 
-{{ review_header(<num>) }}
+{% set series_id = "<series>" %}
+{% set number = <num> %}
 
-{{ review_footer() }}
+{% block review %}
+
+{% endblock %}
 """
 
 TABLE_TEMPLATE = """{% extends "tables/classic/_base.html" %}
@@ -26,15 +30,19 @@ Put synopsis here
 series = sys.argv[1]
 
 templ_dir = Path.cwd() / "templates"
-reviews = sorted((templ_dir / "reviews" / series).iterdir(), key=lambda it: int(it.name[2:5]))
+items = [i for i in (templ_dir / "reviews" / series).iterdir() if i.is_dir()]
+reviews = sorted(items, key=lambda it: int(it.name[2:5]))
 first_ep_n = int(reviews[0].name[2:5])
 next_ep_n = first_ep_n + len(reviews)
 print(f"writing templates for {series} episode {next_ep_n}")
 
-review_f = templ_dir / "reviews" / series / f"ep{next_ep_n:03d}.html"
-review_f.write_text(REVIEW_TEMPLATE.replace("<num>", str(next_ep_n)))
+ep_dir = templ_dir / "reviews" / series / f"ep{next_ep_n:03d}"
+ep_dir.mkdir()
+
+review_f = ep_dir / "index.html"
+review_f.write_text(REVIEW_TEMPLATE.replace("<num>", str(next_ep_n)).replace("<series>", series))
 os.system(f"git add {review_f}")
 
-table_f = templ_dir / "tables" / series / f"ep{next_ep_n:03d}.html"
+table_f = ep_dir / "_table.html"
 table_f.write_text(TABLE_TEMPLATE.replace("<num>", str(next_ep_n)))
 os.system(f"git add {table_f}")
